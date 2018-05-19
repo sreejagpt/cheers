42613cd70889b6be05752ca56d8faf6bc54d66b6
Sreeja G <sreejagpt@gmail.com>
2018-05-19 15:00:17 +1000

    and animated graph lol

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+import Chart from './src/components/Chart';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <VictoryChart/>
+        <Chart/>
       </View>
     );
   }
